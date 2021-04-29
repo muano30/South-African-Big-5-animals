adfaf017faab99141903c05cefff300bcc55bbd4
tumikole <samuelramollo@gmail.com>
2021-04-29 15:58:16 +0200

    changes

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-// Get the elements with class="column"
-var elements = document.getElementsByClassName("column");
-
-// Declare a loop variable
-var i;
-
-// List View
-function listView() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.width = "100%";
-  }
-}
-
-// Grid View
-function gridView() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.width = "50%";
-  }
-}
-
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
